95789e804a032c94cbf54db7d6f497dd7a08fa87
SOUFIANEBENHAYDA <soufianebenhida00@gmail.com>
2025-06-11 16:23:34 +0100

    login function for admin

--- modele/modele_admin.php
+++ modele/modele_admin.php
@@ -1,4 +1,39 @@
 <?php
+function connexion(){
+    try{
+        $pdo=new PDO("mysql:host=localhost;dbname=institute", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }catch(PDOException $er){
+        die("error de connexion: ".$er->getMessage());
+    }
+}
+
+function loginAdmin(){
+    $pdo=connexion();
+    $email=$_POST["email"];
+    $stat=$pdo->prepare("SELECT * FROM Administration where email=:email");
+    $stat->bindParam(":email",$email);
+    $stat->execute();
+    return $stat->fetch();
+}
+function loginAdmin_action(){
+    if(!empty($_POST["password"]) && !empty($_POST["email"])){
+        $res=loginAdmin();
+        if($res!=false){
+            if($res["password"]===$_POST["password"]){
+                session_start();
+                $admin= new Admin($res["ida"], $res["nom"], $res["prenom"],$res["email"],$res["password"]);
+                $_SESSION["admin"]=$admin;
+                header("Location: interfaceAdmin.php");
+                exit();
+            }
+        }else{
+            //echo "email doesn't exit";
+        }
+    }
+    //echo "veullez remplire tous les champs";
+}
 function inscrire(){};
 function ajouter_matiere(){};
 
