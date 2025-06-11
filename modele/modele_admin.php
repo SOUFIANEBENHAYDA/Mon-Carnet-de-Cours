9714ddd82f4caca3524a45cdf9e1803e809682cf
SOUFIANEBENHAYDA <soufianebenhida00@gmail.com>
2025-06-11 16:08:30 +0100

    for class admin

--- modele/modele_admin.php
+++ modele/modele_admin.php
@@ -1,19 +1,33 @@
 <?php
+class Admin{
+    public $email, $pwd;
+    public $nom, $prenom, $ida;
 
+    public function __construct($email, $nom, $prenom, $ida, $pwd)
+    {
+        $this->ida=$ida;
+        $this->nom=$nom;
+        $this->prenom=$prenom;
+        $this->email=$email;
+        $this->pwd=$pwd;
+    }
+    
+    //ajouter des nouveau etudiant
+    public function insciription(){
+        inscrire();
+    }
+    //ajouter des matieres
+    public function AjouterMatiere(){
+        ajouter_matiere();
+    }
+    //ajouter les cours(emploi de temps)
+    public function ajoutercours(){
+        
+    }
+    //les notes
+    public function AjouterNotes(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}
 
 ?>
