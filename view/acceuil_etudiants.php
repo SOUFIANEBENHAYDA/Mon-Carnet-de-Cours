b5338e836f6bf2712b58e92b53a8140bdecc7b78
YOUSSEFIDMASSAOUD <youssefhermano18@gmail.com>
2025-06-12 14:53:18 +0100

    tst

--- view/acceuil_etudiants.php
+++ view/acceuil_etudiants.php
@@ -1,8 +1,35 @@
-<?php
-
-
-echo "<script>alert('welcome')</script>";
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/bootstrap-5.3.3-dist/css/bootstrap-grid.min.css.map">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../fontawesome-free-6.7.1-web/css/all.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand mt-3 p-3">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand "><b>Nom de Web site</b></a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex justify-content-end me-5 " id="collapsibleNavbar">
+                    <ul class="navbar-nav ">
+                        <ul class="nav-item">
+                            <a href="nav-link" class="text-decoration-none text-dark">ABOUT</a>
+                        </ul>
+                        <ul class="nav-item">
+                            <a href="nav-link" class="text-decoration-none text-dark">Emploie</a>
+                        </ul>
+                        <ul class="nav-item">
+                            <a href="nav-link" class="text-decoration-none text-dark">CONTACT</a>
+                        </ul>
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+</body>
+</html>
