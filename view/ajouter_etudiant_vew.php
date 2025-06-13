e27f358524fd75bdd2e7cf7933d552d7a05df810
SOUFIANEBENHAYDA <soufianebenhida00@gmail.com>
2025-06-13 11:44:34 +0100

    test

--- view/ajouter_etudiant_vew.php
+++ view/ajouter_etudiant_vew.php
@@ -13,6 +13,17 @@ $res=Filiere::display()
             <label for="email">Email</label>
             <input type="email" name="email">
 
+            <label for="tel">Telephone</label>
+            <input type="number" name="tel">
+
+            <label for="genre">Femme</label>
+            <input type="radio" name="genre" value="F">
+            <label for="genre">Homme</label>
+            <input type="radio" name="genre" value="M">
+
+            <label for="dn">Date de nissance: </label>
+            <input type="date" name="dn">
+
             <label for="filier">Filiere</label>
             <select name="filier">
                 <?php foreach($res as $f){
@@ -24,6 +35,12 @@ $res=Filiere::display()
                 <option value="1">1er</option>
                 <option value="2">2er</option>
             </select>
+
+            <label for="pwd"></label>
+            <input type="password" name="pwd">
+
+            <label for="image">Image</label>
+            <input type="file" name="photo">
 
             <button type="submit">Inscrire</button>
         </form>
