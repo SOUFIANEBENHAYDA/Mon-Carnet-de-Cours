edc1c2a743e94728dfbf2a861804b614b655c982
SOUFIANEBENHAYDA <soufianebenhida00@gmail.com>
2025-06-17 01:11:42 +0100

    adding somthing

---
+++ view/ajouter_emploi_view.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<html>
+    <body>
+        <table>
+            <tr><td></td><td>8:30-11:00</td><td>11:00-13:30</td><td>13:30-16:00</td><td>16:00-18:30</td></tr>
+            <tr><td>Lundi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+            <tr><td>Mardi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+            <tr><td>Mercredi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+            <tr><td>Jeudi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+            <tr><td>Vendredi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+            <tr><td>Samedi</td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td><td><input type="text" name=""></td></tr>
+        </table>
+    </body>
+</html>
